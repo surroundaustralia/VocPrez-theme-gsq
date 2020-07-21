f846618919a376660b980a4c6eca98024bf33d0d
Nicholas Car <nicholas.car@surroundaustralia.com>
2020-07-21 11:28:09 +1000

    Update apply.sh

--- apply.sh
+++ apply.sh
@@ -6,11 +6,4 @@ cp $VP_THEME_HOME/style/* $VP_HOME/vocprez/view/style
 echo "copying $VP_THEME_HOME/templates content to $VP_HOME/vocprez/view/templates"
 cp $VP_THEME_HOME/templates/* $VP_HOME/vocprez/view/templates
 
-# append paths to VP routes, removing running locally commands
-echo "adding new routes to $VP_HOME/vocprez/app.py"
-# truncate app.py
-sed -i -n '/# run the Flask app/q;p' $VP_HOME/vocprez/app.py
-# add new routes
-more $VP_THEME_HOME/routes.txt >> $VP_HOME/vocprez/app.py
-
 echo "customisation done"
