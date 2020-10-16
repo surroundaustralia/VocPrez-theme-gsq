ef354b865f75cd22509f08a0b401b4e9a62fa8db
Nicholas Car <nicholas.car@surroundaustralia.com>
2020-10-16 12:21:00 +1000

    Update apply.sh

--- apply.sh
+++ apply.sh
@@ -6,4 +6,10 @@ cp $VP_THEME_HOME/style/* $VP_HOME/vocprez/view/style
 echo "copying $VP_THEME_HOME/templates content to $VP_HOME/vocprez/view/templates"
 cp $VP_THEME_HOME/templates/* $VP_HOME/vocprez/view/templates
 
+echo "Config"
+echo "Alter $VP_THEME_HOME/config.py to include variables"
+sed 's#$SPARQL_ENDPOINT#'"$SPARQL_ENDPOINT"'#' $VP_THEME_HOME/config.py > $VP_THEME_HOME/config_updated.py
+echo "Move $VP_THEME_HOME/config.py to $VP_HOME/vocprez/_config/__init__.py"
+mv $VP_THEME_HOME/config_updated.py $VP_HOME/vocprez/_config/__init__.py
+
 echo "customisation done"
