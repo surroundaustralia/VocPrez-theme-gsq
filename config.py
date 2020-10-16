50ba4fc8bb4c1552c08e8819aa4519f1780d2a4c
Nicholas Car <nicholas.car@surroundaustralia.com>
2020-10-16 12:20:35 +1000

    Create config.py

---
+++ config.py
@@ -0,0 +1,55 @@
+from os import path
+
+APP_DIR = path.dirname(path.dirname(path.realpath(__file__)))
+SKIN_DIR = path.join(APP_DIR, "view")
+TEMPLATES_DIR = path.join(SKIN_DIR, "templates")
+STATIC_DIR = path.join(SKIN_DIR, "style")
+LOGFILE = APP_DIR + "/flask.log"
+CACHE_FILE = path.join(APP_DIR, "cache", "DATA.p")
+CACHE_HOURS = 1
+DEFAULT_LANGUAGE = "en"
+SPARQL_QUERY_LIMIT = 2000  # Maximum number of results to return per SPARQL query
+MAX_RETRIES = 2
+RETRY_SLEEP_SECONDS = 10
+SPARQL_TIMEOUT = 60
+LOCAL_URLS = True  # Parameter governing whether URLs shown are local or external
+
+
+#
+#   Vocabulary data sources
+#
+# Here is the list of vocabulary sources that this instance uses. FILE, SPARQL, RVA & VOCBENCH are implemented already
+# and are on by default (e.g. VOCBENCH = None) but other sources, such as GitHub can be added. To enable them, add a new
+# like like VocBench.XXX = None
+class VocabSource:
+    FILE = "FILE"
+    SPARQL = "SPARQL"
+    RVA = "RVA"
+    VOCBENCH = "VOCBENCH"
+    GITHUB = "GITHUB"
+
+# Main cache (SPARQL DB) variables
+# BEGIN Instance Vars
+DEBUG = False
+SPARQL_ENDPOINT = "$SPARQL_ENDPOINT"
+SPARQL_USERNAME = ""
+SPARQL_PASSWORD = ""
+SOURCE_NAME = "gsq"
+# END Instance Vars
+
+DATA_SOURCES = {
+    # example SPARQL source configured using variables in "Instance Vars" above
+    SOURCE_NAME: {
+        "source": VocabSource.SPARQL,
+        "sparql_endpoint": SPARQL_ENDPOINT,
+        "sparql_username": SPARQL_USERNAME,
+        "sparql_password": SPARQL_PASSWORD,
+    },
+}
+
+# Details for the main vocabulary list. Can be overridden in the vocabularies.html template
+# BEGIN Vocabs list info
+VOCS_URI = "https://vocabs.gsq.digital/vocab/"
+VOCS_TITLE = "GSQ Vocabularies"
+VOCS_DESC = "Vocabularies managed by the Geological Survey of Queensland"
+# END Vocabs list info
